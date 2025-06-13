7c427828d006bde95382108de953d153a6d15564
Adithya Awati <aditya.n.awati@gmail.com>
2025-06-13 09:16:04 +0000

    update: add query parameter of Enum model validation type to list models endpoint

--- src/api/routers/model.py
+++ src/api/routers/model.py
@@ -5,15 +5,22 @@ from fastapi import APIRouter, Depends, HTTPException, Path
 from api.auth import api_key_auth
 from api.models.bedrock import BedrockModel
 from api.schema import Model, Models
+from typing import Optional
+from enum import Enum
 
 router = APIRouter(
     prefix="/models",
-    dependencies=[Depends(api_key_auth)],
+    # dependencies=[Depends(api_key_auth)],
     # responses={404: {"description": "Not found"}},
 )
 
 chat_model = BedrockModel()
 
+class ModelType(str, Enum):
+    TEXT = "TEXT"
+    IMAGE = "IMAGE"
+    VIDEO = "VIDEO"
+    EMBEDDING = "EMBEDDING"
 
 async def validate_model_id(model_id: str):
     if model_id not in chat_model.list_models():
@@ -21,8 +28,9 @@ async def validate_model_id(model_id: str):
 
 
 @router.get("", response_model=Models)
-async def list_models():
-    model_list = [Model(id=model_id) for model_id in chat_model.list_models()]
+async def list_models(model_type: Optional[ModelType] = None):
+    model_type_str = model_type.value if model_type else None
+    model_list = [Model(id=model_id) for model_id in chat_model.list_models(model_type)]
     return Models(data=model_list)
 
 
